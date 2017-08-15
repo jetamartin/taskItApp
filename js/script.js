2205b7e3026e97236de4e3f738cfd373cdc89673
Jet Martin <jetamartin@yahoo.com>
2017-08-14 22:54:51 -0700

    Moved completed? checkbox on task to right side and added confirmation modal window for marking task as complte

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-$('#datepicker').datepicker();
+//$('#datepicker').datepicker();
+
+// Note: that the taskItController & uiControllers are designed to be independent.
+// This is done so that for example you can expand the capabilities of the
+// taskItController without affecting the UI.
+//*********************************************************************
+//
+// TASKIT CONTROLLER 
+//
+//*********************************************************************
+
+var taskItModel = (function () {
+
+})();
+
+
+//*********************************************************************
+//
+// UI CONTROLLER
+//
+//*********************************************************************
+
+var uiController = (function () {
+	var DOMstrings = {
+
+	};
+
+})();
+
+
+
+// AppController connects the taskItModel and UIcontrollers together;
+// It wasn't necessary to pass other controllers in as params but doing so
+// creates more independence and separation of control. Also note that 
+// taskIt and ui controller param names are slightly diff than names of these controllers
+
+//*********************************************************************
+//
+// GENERAL APP CONTROLLER
+//
+//*********************************************************************
+
+var appController = (function (taskItMdl, UICtrl) {
+
+
+})(taskItController, uiController);
