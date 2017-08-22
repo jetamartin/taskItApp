cd6af666456523601919841e73157c99e8dde5e2
Jet Martin <jetamartin@yahoo.com>
2017-08-22 11:51:22 -0700

    JS for controlling search...not completely correct

--- js/script.js
+++ js/script.js
@@ -6,23 +6,56 @@
 //	}
 //}
 
+function toggleClass(element, className) {
+	if (!element || !className) {
+		return;
+	}
+
+	var classString = element.className,
+		nameIndex = classString.indexOf(className);
+	if (nameIndex == -1) {
+		classString += ' ' + className;
+	} else {
+		classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
+	}
+	element.className = classString;
+}
+
+function isEmpty(idValue) {
+	//	console.log("Search length value: ", document.getElementById(idValue).value.length);
+	if (document.getElementById(idValue).value.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
+	//	console.log("Search Form length: ", document.forms["searchForm"].search.length);
+	//	if (document.forms["searchForm"].search.length === 0) {
+	//		console.log("Search value: ", document.forms["searchForm"].search.value);
+	//		return true;
+	//	} else {
+	//		return false;
+	//	}
+}
+
+//*****************************************
+//
+//  GLOBAL VARIABLE DECLARATION
+//
+//*****************************************
+
+
 var previouslySelectedList;
 var parentElem;
+var initialInput = true;
+var clearSearchClicked = false;
 
-function toggleClass(element, className){
-    if (!element || !className){
-        return;
-    }
+var listMenuElement = document.getElementById('taskListDropdown');
+var sysMenuElement = document.querySelector('.sysMenu');
+var clearSearchBox = document.querySelector('.clearSearchBox');
+var searchInput = document.getElementById("search");
 
-    var classString = element.className, nameIndex = classString.indexOf(className);
-    if (nameIndex == -1) {
-        classString += ' ' + className;
-    }
-    else {
-        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-    }
-    element.className = classString;
-}
+toggleClass(clearSearchBox, "hideIt");
+
 
 //document.getElementById('YOUR-BUTTON').addEventListener('click', function() {
 //    toggleClass(document.getElementById('ELEMENT TO BE CHANGED'), 'YOUR-CLASS');
@@ -135,45 +168,120 @@ for (var i = 0; i < userDefinedTaskListName.length; i++) {
 var handleSubMenuClick = function (event) {
 	// Get name of submenu list selected
 	var listNameSelected = event.target.childNodes[1].textContent;
-	
-	console.log("Event.target = ", event.target);
+
+	//	console.log("Event.target = ", event.target);
 	// Get handle for current List Menu 
-	var listMenuElement = document.getElementById('taskListDropdown');
-	
+
+
 	// Get location of List menu title 
 	var listMenuTitle = listMenuElement.childNodes[2];
-	
-	console.log(listMenuElement.childNodes);
-	
+
+	//	console.log(listMenuElement.childNodes);
+
 	// Make the List menu title equal to submenu name selected
 	listMenuTitle.nodeValue = listNameSelected;
-	
-	// Toggle previously selected list item to remove "selected" class & remove the darkended backgroun
-	toggleClass(previouslySelectedList,'selected');
-	
+
+	// Toggle previously selected list item to remove "selected" class & remove the darkended background
+	toggleClass(previouslySelectedList, 'selected');
+
 	// The selected list name will have the "selected" class added to darken background so when 
 	// user hovers and gets the submenu to display the previously selected list will be distinguishable 
 	toggleClass(event.target, 'selected');
-	
+
 	// Save the selected list element in previouslySelectedList variable 
 	previouslySelectedList = event.target;
-	
+
 	// *****************************************************************************************
 	// Code below was attempt to hide submenu once user click on one of the list in the submenu
 	//******************************************************************************************	
 	// Get the parent of submenu so that I can hide it
-	parentElem = event.target.parentElement;	
+	parentElem = event.target.parentElement;
 	//  Add hideIt class so that 	
 	//	toggleClass(parentElem,"hideIt"); 
 };
 
-var handleSearchClick = function (event) {
-/* .searchIt input[type="text"]:focus	*/
-	if (event.target === document.activeElement) {
-		console.log("Search is active element");
-	};
+function whereDidYouGo() {
+	return document.activeElement;
+}
+
+var handleSearchFocus = function (event) {
+	console.log("In handleSearchFocus function");
+	//	console.log("Event.target: ", event.target, event.target.tagName);
+	//	console.log("RelatedTarget: ", event.relatedTarget);
+	//	console.log("Active Element: ", document.activeElement);
+	//	console.log("Results-Where did you go: ", whereDidYouGo());
+	if (!clearSearchClicked) {
+		toggleClass(listMenuElement, "hideIt");
+		toggleClass(sysMenuElement, "hideIt");
+		toggleClass(clearSearchBox, "invisible");
+		toggleClass(clearSearchBox, "hideIt");
+	}
 };
 
+var handleSearchBlur = function (event) {
+	clearSearchClicked = false;
+
+	console.log("In handleSearchBlur function");
+	//	console.log("Results-Where did you go: ", whereDidYouGo());
+
+	console.log("Event: ", event);
+	setTimeout(function () {
+		console.log("----->EVENT THAT FIRED", document.activeElement);
+		if (!clearSearchClicked) {
+			toggleClass(listMenuElement, "hideIt");
+			toggleClass(sysMenuElement, "hideIt");
+			toggleClass(clearSearchBox, "hideIt");
+		} else {
+			searchInput.focus();
+		}
+	}, 0);
+
+	// After input field is in focus and you click out of it (click on "x") you want to clear input field
+	//	document.getElementById("search").value = null;
+
+	// Put Search input field back into focus so users can enter another search criteria
+	//	searchInput.focus();
+
+	// Re-hide clear search box icon 
+	//	toggleClass(clearSearchBox, "invisible");
+
+
+
+};
+
+var detectSearchInput = function (event) {
+	console.log(document.getElementById("search").value.length);
+	console.log("InitialInput value: ", initialInput);
+	if (initialInput) {
+		toggleClass(clearSearchBox, "invisible");
+		initialInput = false;
+	}
+
+	if (isEmpty("search")) {
+		initialInput = true;
+		toggleClass(clearSearchBox, "invisible");
+	}
+};
+
+var clearSearchField = function (event) {
+	initialInput = true;
+	clearSearchClicked = true;
+	console.log("ClearSearchField");
+	console.log("ClearSearchField target === ", event.target);
+
+
+	// After input field is in focus and you click out of it (click on "x") you want to clear input field
+	document.getElementById("search").value = null;
+
+
+	// Re-hide clear search box icon 
+	toggleClass(clearSearchBox, "invisible");
+
+	// Put Search input field back into focus so users can enter another search criteria
+	//	searchInput.focus();
+	//	document.getElementById("search").value = "";
+	//	toggleClass(clearSearchBox, "invisible");
+};
 //*********************************************************************
 //
 //* SET UP EVENTLISTENERS
@@ -183,8 +291,11 @@ var handleSearchClick = function (event) {
 // EventListener for List Submenu 
 document.querySelector(".subMenuElements").addEventListener('click', handleSubMenuClick);
 
-// Event Listener for Search 
-document.querySelector(".searchIt").addEventListener('click', handleSearchClick);
+// Event Listener for Search onFocus and OnBlur
+document.querySelector("#search").addEventListener("focus", handleSearchFocus);
+document.querySelector("#search").addEventListener("blur", handleSearchBlur);
+document.querySelector("#search").addEventListener("keyup", detectSearchInput);
+document.querySelector(".clearSearchBox").addEventListener("click", clearSearchField);
 
 
 //$('#datepicker').datepicker();
